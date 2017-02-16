1ae403702efc3b3eb8ee398c657523b38e7e5fb9
Scott Petersen <scott@nutech.com>
2017-02-16 11:46:25 -0800

    Initial commit.

---
+++ playground/Scenes/Playground.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Nez;
+using Nez.Sprites;
+
+using System;
+using System.Collections.Generic;
+using Nez.UI;
+using Nez.Tiled;
+
+namespace playground.Scenes
+{
+    class Test : Scene
+    {
+        #region Member variables
+        #endregion
+
+        #region Events
+        public override void initialize()
+        {
+            base.initialize();
+
+            Core.debugRenderEnabled = true;
+			Transform.shouldRoundPosition = false;
+
+            addRenderer<DefaultRenderer>(new DefaultRenderer());
+        }
+
+        public override void onStart()
+        {
+            base.onStart();
+        }
+
+        public override void update()
+        {
+            base.update();
+        }
+        #endregion
+
+        #region Public interface
+        #endregion
+
+        #region Private
+        #endregion
+    }
+}
