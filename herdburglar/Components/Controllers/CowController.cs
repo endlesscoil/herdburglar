930f3d22e13c3afea2433fc2b3a513ee5cc9306b
Scott Petersen <scott@nutech.com>
2017-02-20 12:24:12 -0800

    Think the cow facing stuff is correct now.. mostly.. still dont know why it's pi and not 2pi when calculating angle.

--- herdburglar/Components/Controllers/CowController.cs
+++ herdburglar/Components/Controllers/CowController.cs
@@ -12,16 +12,16 @@ namespace herdburglar.Components.Controllers
 {
     class CowController : Component, IUpdatable
     {
-        public static Dictionary<Orientation, Vector2> orientationToFacingDirection = new Dictionary<Orientation, Vector2>
+		public static Dictionary<Cow.Orientation, Vector2> orientationToFacingDirection = new Dictionary<Cow.Orientation, Vector2>
         {
-            { Orientation.Up, new Vector2(0, 1) },
-            { Orientation.Left, new Vector2(1, 0) },
-            { Orientation.Down, new Vector2(0, -1) },
-            { Orientation.Right, new Vector2(-1, 0) }
+			{ Cow.Orientation.Up, new Vector2(0, -1) },
+			{ Cow.Orientation.Left, new Vector2(-1, 0) },
+			{ Cow.Orientation.Down, new Vector2(0, 1) },
+			{ Cow.Orientation.Right, new Vector2(1, 0) }
         };
 
         private Cow cow = null;
-        private float fovAngle = MathHelper.Pi / 4; // 45deg
+        private float fovAngle = MathHelper.Pi / 4; // 90 degrees
         private float _computedAngle;
         private float alertDistance = 250f;
         private float dangerDistance = 175;
@@ -36,24 +36,34 @@ namespace herdburglar.Components.Controllers
 
         void IUpdatable.update()
         {
+            var facingDirection = orientationToFacingDirection[cow.orientation];
+
+			// Draw facing indicator
+			if (Core.debugRenderEnabled)
+				Debug.drawLine(entity.transform.position, entity.transform.position + (facingDirection * 100), Color.Blue, 0.5f);
+
+        	// Find the burglar
             var burglar = entity.scene.findEntitiesWithTag((int)Tags.Burglar);
             if (burglar.Count > 0)
             {
+            	// Are they within alert distance?
                 var distance = Vector2.Distance(entity.transform.position, burglar[0].transform.position);
                 if (distance < alertDistance)
                 {
-                    var facingDirection = orientationToFacingDirection[cow.orientation];
-                    var vector_to_burglar = Vector2.Normalize(entity.transform.position - burglar[0].transform.position);
+					var vector_to_burglar = Vector2.Normalize(burglar[0].transform.position - entity.transform.position);
                     var dot = Vector2.Dot(facingDirection, vector_to_burglar);
 
+                    // Are they in front of us?
                     if (dot > _computedAngle)
                     {
                         var raycastHit = Physics.linecast(entity.transform.position, burglar[0].transform.position);
 
+                        // And can we actually see them?
                         if (raycastHit.collider.entity.tag == (int)Tags.Burglar)
                         {
                             var color = distance < dangerDistance ? Color.Red : Color.Yellow;
-                            Debug.drawLine(entity.transform.position, burglar[0].transform.position, color, 0.5f);
+							if (Core.debugRenderEnabled)
+                            	Debug.drawLine(entity.transform.position, burglar[0].transform.position, color, 0.5f);
 
                             // TODO: Do something here for alert/danger.
                         }
