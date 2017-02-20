435768a0db2029de4e012bea67fc2b05f031537d
Scott Petersen <scott@nutech.com>
2017-02-20 11:31:49 -0800

    Mostly works in linux..

---
+++ herdburglar/Components/Controllers/IdolController.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using Nez;
+
+using Nez.Sprites;
+using System.Collections.Generic;
+
+namespace herdburglar.Components.Controllers
+{
+    class IdolController : Component
+    {
+
+    }
+}
