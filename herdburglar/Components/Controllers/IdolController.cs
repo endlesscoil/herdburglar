a6727f414efca6d98570e18bfb0239e47ee396d2
Scott Petersen <scott@digression.org>
2017-02-20 19:07:21 -0800

    Feh

--- herdburglar/Components/Controllers/IdolController.cs
+++ herdburglar/Components/Controllers/IdolController.cs
@@ -1,15 +1,43 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Nez;
 
-using Nez.Sprites;
-using System.Collections.Generic;
-
 namespace herdburglar.Components.Controllers
 {
-    class IdolController : Component
+    class IdolController : Component, ITriggerListener
     {
+        private BoxCollider collider;
 
+        public override void onAddedToEntity()
+        {
+            base.onAddedToEntity();
+
+            collider = new BoxCollider(32, 32);
+            collider.isTrigger = true;
+
+            entity.addComponent(collider);
+        }
+
+        void ITriggerListener.onTriggerEnter(Collider other, Collider self)
+        {
+            if (other.entity.tag == (int)Tags.Burglar)
+            {
+                Debug.log("Idol picked up by burglar!");
+
+                // TODO: game win, etc.
+
+                // NOTE: Why does this crash shit?
+                //self.entity.destroy();
+            }
+        }
+
+        void ITriggerListener.onTriggerExit(Collider other, Collider local)
+        {
+
+        }
     }
 }
