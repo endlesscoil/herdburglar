7adec8cb3bc5affddeb90da7165a5c2dfa719b2e
Scott Petersen <scott@nutech.com>
2017-03-23 15:30:16 -0700

    Made map into a full class.

--- herdburglar/Scenes/Playground.cs
+++ herdburglar/Scenes/Playground.cs
@@ -17,6 +17,7 @@ namespace herdburglar
     class Playground : Scene
     {
         #region Member variables
+        herdburglar.Map map;
         #endregion
 
         #region Events
@@ -40,7 +41,8 @@ namespace herdburglar
         {
 			base.onStart();
 
-			load_tiled_map();
+            map = new herdburglar.Map() { backgroundRenderLayer = 10, foregroundRenderLayer = -1 };
+            map.load(@"maps/test", "tiled-map-entity", "Tile Layer 1", "Tile Layer 2", "collision", "spawns");
         }
 
         public override void update()
@@ -53,98 +55,6 @@ namespace herdburglar
         #endregion
 
         #region Private
-        private void load_tiled_map()
-        {
-            var mapEntity = createEntity("tiled-map-entity");
-            var map = content.Load<TiledMap>(@"maps/test");
-
-            // Load background layer
-            var mapComponent = mapEntity.addComponent(new TiledMapComponent(map));
-            mapComponent.setLayersToRender(new string[] { "Tile Layer 1" });
-            mapComponent.renderLayer = 10;
-
-            // Load foreground layer
-            var mapDetailComponent = mapEntity.addComponent(new TiledMapComponent(map));
-            mapDetailComponent.setLayersToRender(new string[] { "Tile Layer 2" });
-            mapDetailComponent.renderLayer = -1;
-
-            // Add colliders from collider layer
-            add_map_colliders(map, mapEntity);
-
-            // Spawn entities at spawn points.
-            spawn_stuff(map);
-        }
-
-        private void add_map_colliders(TiledMap map, Entity entity)
-        {
-            var col_layer = map.getObjectGroup("collision");
-
-            for (var i = 0; i < col_layer.objects.Length; i++)
-            {
-                var obj = col_layer.objects[i];
-                var c = new BoxCollider(obj.x, obj.y, obj.width, obj.height);       // TODO: add support for collider shapes that aren't boxes.
-
-                entity.addComponent(c);
-            }
-        }
-
-        private void spawn_stuff(TiledMap map)
-        {
-            var spawn_layer = map.getObjectGroup("spawns");
-            for (var i = 0; i < spawn_layer.objects.Length; i++)
-            {
-                var obj = spawn_layer.objects[i];
-                var objPosition = new Vector2(obj.position.X + obj.width / 2, obj.position.Y + obj.height / 2);
-
-                switch (obj.type)
-                {
-                    case "BurglarSpawn":
-                    {
-                        var burglar = new Burglar();
-                        burglar.transform.position = objPosition;
-                        addEntity(burglar);
-                        break;
-                    }
-
-                    case "CowSpawn":
-                    {
-                        var orientation = CowController.Orientation.Right;
-                        if (obj.properties.ContainsKey("orientation"))
-                            orientation = CowController.getOrientationFromName(obj.properties["orientation"]);
-
-                        var cow = new Cow(orientation: orientation);
-                        cow.transform.position = objPosition;
-                        cow.name = obj.name;
-                        addEntity(cow);
-                        break;
-                    }
-
-                    case "BullSpawn":
-                    {
-                        var orientation = CowController.Orientation.Right;
-                        if (obj.properties.ContainsKey("orientation"))
-                            orientation = CowController.getOrientationFromName(obj.properties["orientation"]);
-
-                        var bull = new Bull(orientation: orientation);
-                        bull.transform.position = objPosition;
-                        bull.name = obj.name;
-                        addEntity(bull);
-                        break;
-                    }
-
-                    case "IdolSpawn":
-                    {
-                        var idol = new Idol();
-                        idol.transform.position = objPosition;
-                        addEntity(idol);
-                        break;
-                    }
-
-                    default:
-                        break;
-                }
-            }
-        }
         #endregion
     }
 }
--- herdburglar/Util/Map.cs
+++ herdburglar/Util/Map.cs
@@ -4,11 +4,44 @@ using Microsoft.Xna.Framework;
 using Nez;
 using Nez.Tiled;
 
+using herdburglar.Components.Controllers;
+
 namespace herdburglar
 {
     class Map
     {
-        public static TiledTile getTileAt(TiledMap map, Vector2 pos)
+        public int backgroundRenderLayer = 0;
+        public int foregroundRenderLayer = 0;
+
+        private TiledMap map;
+        private Entity mapEntity;
+        private Component backgroundLayer;
+        private Component foregroundLayer;
+
+        #region Public
+        public bool load(string mapResource, string targetEntity, 
+                         string backgroundLayerName = null, string foregroundLayerName = null, 
+                         string collisionLayerName = null, string spawnLayerName = null)
+        {
+            mapEntity = Core.scene.createEntity(targetEntity);
+            map = Core.content.Load<TiledMap>(mapResource);
+
+            if (backgroundLayerName != null)
+                backgroundLayer = loadLayer(backgroundLayerName, backgroundRenderLayer);
+
+            if (foregroundLayerName != null)
+                foregroundLayer = loadLayer(foregroundLayerName, foregroundRenderLayer);
+
+            if (collisionLayerName != null)
+                addMapColliders(collisionLayerName);
+
+            if (spawnLayerName != null)
+                spawn(spawnLayerName);
+
+            return true;
+        }
+
+        public TiledTile getTileAt(Vector2 pos)
         {
             TiledTile ret = null;
 
@@ -23,18 +56,18 @@ namespace herdburglar
             return ret;
         }
 
-        public static Vector2 getTileAt_Center(TiledMap map, Vector2 pos)
+        public Vector2 getTileCenterAt(Vector2 pos)
         {
-            var tile = getTileAt(map, pos);
+            var tile = getTileAt(pos);
             var world_pos = tile.getWorldPosition(map);
             var offset = new Vector2(map.tileWidth / 2, map.tileHeight / 2);
-            
+
             return new Vector2(world_pos.X + offset.X, world_pos.Y + offset.Y);
         }
 
-        public static bool hasCollider(TiledMap map, Vector2 pos)
+        public bool hasCollider(Vector2 pos)
         {
-            var tile = getTileAt(map, pos);
+            var tile = getTileAt(pos);
             var tile_rect = tile.getTileRectangle(map);
             var tile_rectf = new RectangleF(tile_rect.X, tile_rect.Y, tile_rect.Width, tile_rect.Height);
 
@@ -51,7 +84,7 @@ namespace herdburglar
             return false;
         }
 
-        public static List<Point> getOpenTiles(TiledMap map)
+        public List<Point> getOpenTiles()
         {
             List<Point> openTiles = new List<Point>();
 
@@ -60,14 +93,92 @@ namespace herdburglar
                 for (int y = 0; y < map.height; y++)
                 {
                     var p = new Point(x, y);
-                    if (!hasCollider(map, map.tileToWorldPosition(p)))
-                    {
+
+                    if (!hasCollider(map.tileToWorldPosition(p)))
                         openTiles.Add(p);
-                    }
                 }
             }
 
             return openTiles;
         }
+        #endregion
+
+        #region Private
+        private Component loadLayer(string layerName, int renderLayer = 0)
+        {
+            var component = mapEntity.addComponent(new TiledMapComponent(map));
+            component.setLayersToRender(new string[] { layerName });
+            component.renderLayer = renderLayer;
+
+            return component;
+        }
+
+        private void addMapColliders(string collisionLayerName)
+        {
+            var col_layer = map.getObjectGroup(collisionLayerName);
+
+            for (var i = 0; i < col_layer.objects.Length; i++)
+            {
+                var obj = col_layer.objects[i];
+                var c = new BoxCollider(obj.x, obj.y, obj.width, obj.height);       // TODO: add support for collider shapes that aren't boxes.
+
+                mapEntity.addComponent(c);
+            }
+        }
+
+        private void spawn(string spawnLayerName)
+        {
+            var spawn_layer = map.getObjectGroup(spawnLayerName);
+            for (var i = 0; i < spawn_layer.objects.Length; i++)
+            {
+                var obj = spawn_layer.objects[i];
+                var objPosition = new Vector2(obj.position.X + obj.width / 2, obj.position.Y + obj.height / 2);
+
+                switch (obj.type)
+                {
+                    case "BurglarSpawn": {
+                        var burglar = new Burglar();
+                        burglar.transform.position = objPosition;
+                        Core.scene.addEntity(burglar);
+                        break;
+                    }
+
+                    case "CowSpawn": {
+                        var orientation = CowController.Orientation.Right;
+                        if (obj.properties.ContainsKey("orientation"))
+                            orientation = CowController.getOrientationFromName(obj.properties["orientation"]);
+
+                        var cow = new Cow(orientation: orientation);
+                        cow.transform.position = objPosition;
+                        cow.name = obj.name;
+                        Core.scene.addEntity(cow);
+                        break;
+                    }
+
+                    case "BullSpawn": {
+                        var orientation = CowController.Orientation.Right;
+                        if (obj.properties.ContainsKey("orientation"))
+                            orientation = CowController.getOrientationFromName(obj.properties["orientation"]);
+
+                        var bull = new Bull(orientation: orientation);
+                        bull.transform.position = objPosition;
+                        bull.name = obj.name;
+                        Core.scene.addEntity(bull);
+                        break;
+                    }
+
+                    case "IdolSpawn": {
+                        var idol = new Idol();
+                        idol.transform.position = objPosition;
+                        Core.scene.addEntity(idol);
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+            }
+        }
+        #endregion
     }
 }
